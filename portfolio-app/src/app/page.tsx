4c9e1040b44ed4abd12ae51191c3e8e26fb0e27d
marvix1122333 <iyanu.olamide01@gmail.com>
2026-07-25 02:30:37 +0100

    Initial portfolio commit

---
+++ portfolio-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { SplineSceneBasic } from "@/components/demo";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-black font-sans min-h-screen p-8">
+      <SplineSceneBasic />
+    </div>
+  );
+}
